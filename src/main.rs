72a5e23f25770f032edad37f37c6ac1efc8b44f5
Nguyen Tuong Phat <tuongphat2505@gmail.com>
2025-10-12 16:45:44 +0800

    put panic function into separate module

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,11 @@
 extern crate alloc;
 extern crate aos_uefi;
 
+mod panic;
 mod system;
 mod wrappers;
 
 use aos_uefi::{Handle, status::Status, system::SystemTable};
-use core::panic::PanicInfo;
 
 use crate::{system::System, wrappers::stdio::Stdout};
 
@@ -51,22 +51,5 @@ fn main() -> Result<(), Status> {
         println!("Found entry\n{}", entry);
     }
     println!("I made it here!");
-    loop {}
-}
-
-#[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
-    if let Some(&l) = _info.location() {
-        println!(
-            "At file: {}, line {}, column {}",
-            l.file(),
-            l.line(),
-            l.column()
-        );
-    }
-    println!(
-        "Panicked: {}",
-        _info.message().as_str().unwrap_or("No message...")
-    );
     loop {}
 }
---
+++ src/panic.rs
@@ -0,0 +1,20 @@
+use core::panic::PanicInfo;
+
+use crate::println;
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    if let Some(&l) = _info.location() {
+        println!(
+            "At file: {}, line {}, column {}",
+            l.file(),
+            l.line(),
+            l.column()
+        );
+    }
+    println!(
+        "Panicked: {}",
+        _info.message().as_str().unwrap_or("No message...")
+    );
+    loop {}
+}
